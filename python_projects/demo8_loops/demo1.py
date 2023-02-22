1ce3df8f952dc227415e3d81b3e15a026d268276
Maulin Desai <Maulin.Desai@einfochips.com>
2023-02-22 11:52:47 +0530

    test_0222

---
+++ python_projects/demo8_loops/demo1.py
@@ -0,0 +1,11 @@
+colours = ["red", "green", "yellow", "blue"]
+
+for i in range(0, len(colours)):
+    print(colours[i])
+
+numbers = [45, 98, 75, 65, 24, 88, 74, 56, 75]
+for n in range(0, len(numbers)):
+    if numbers[n] <= 50:
+        print(numbers[n])
+
+
