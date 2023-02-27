753f5d37b364f4f60ab7146eb38737c6d55d6937
Maulin Desai <Maulin.Desai@einfochips.com>
2023-02-27 13:26:12 +0530

    day9_worksession

--- python_projects/Day1_2_assignments/math_runner/runner.py
+++ python_projects/Day1_2_assignments/math_runner/runner.py
@@ -1,11 +1,9 @@
-import math
-
-from math_article import volume
+from Day1_2_assignments.math_article import volume
 
 res = volume.volume_of_sphere(10)
 res = str(round(res,2))
 print(res)
 
-res = volume.volume_of_cylinder(10,2)
+res = volume.volume_of_cylinder(10, 2)
 res = str(round(res,2))
 print(res)
