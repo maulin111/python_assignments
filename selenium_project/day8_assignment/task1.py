3f31d36c4b0c870f2edf16abedb2c74c1672d869
Maulin Desai <Maulin.Desai@einfochips.com>
2023-02-27 10:46:15 +0530

    day9_workk

--- selenium_project/day8_assignment/task1.py
+++ selenium_project/day8_assignment/task1.py
@@ -1,25 +1,28 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.command import Command
 
-driver = webdriver.Chrome()
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge()
 driver.maximize_window()
 driver.implicitly_wait(10)
 driver.get("https://www.oracle.com/in/database/")
-print(driver)
+print(driver.session_id)
 driver.find_element(By.ID, 'acctBtnLabel').click()
-driver.find_element(By.XPATH, '//a[normalize-space()="Sign-In"]').click()
-# abc = driver.find_element(By.XPATH,"//h2[normalize-space()='Oracle account sign in']")
-# driver.switch_to.active_element(abc)
-time.sleep(2)
+driver.find_element(By.PARTIAL_LINK_TEXT,"Sign-In").click()
 
-print("test1")
+print(driver.title)
+print(driver.current_url)
+
+header=driver.find_element(By.XPATH,"//h2").text
+print(header)
+
 driver.find_element(By.ID, 'sso_username').send_keys("Maulin")
 driver.find_element(By.ID, 'ssopassword').send_keys("desai")
 driver.find_element(By.ID, 'signin_button').click()
-error = driver.find_element(By.XPATH, '//span[@id="errormsg"]//div[1]')
-# assert error == "Invalid username and/or password."
-# print(error, 'pass')
-time.sleep(1)
-driver.quit()
+time.sleep(3)
+error = driver.find_element(By.XPATH,"//div[contains(text(),'Invalid')]").text
+assert error == "Invalid username and/or password."
+
+print(error, 'pass')
+
