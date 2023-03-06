28441e1a40e5fda2702a55f1ae9fad5c40e9c7eb
Maulin111 <Maulin.desai@einfochips.com>
2023-03-06 12:32:57 +0530

    Day12_worksession

---
+++ Hybrid_framework/base/webdriver_listner.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+
+class WebDriverWrapper:
+    @pytest.fixture(scope="function", autouse=True)
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
--- Hybrid_framework/test/test_login.py
+++ Hybrid_framework/test/test_login.py
@@ -7,30 +7,19 @@ from assertpy import assert_that
 from selenium.webdriver.support import wait
 from selenium.webdriver.support.wait import WebDriverWait
 
+from base.webdriver_listner import WebDriverWrapper
 
-class TestLogin:
+
+class TestLogin(WebDriverWrapper):
     def test_validlogin(self):
-        print("valid login")
+        self.driver.find_element(By.XPATH, "//input[@placeholder='Username']").send_keys("Admin")
+        self.driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
 
-class TestLoginUI:
 
-    @pytest.fixture(scope="function", autouse=True)
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(20)
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        yield
-        self.driver.quit()
-
+class TestLoginUI(WebDriverWrapper):
     def test_header(self):
         title = self.driver.find_element(By.XPATH, "//h5[normalize-space()='Login']").text
         assert_that("Login").is_equal_to(title)
-
-    def test_validlogin(self):
-        self.driver.find_element(By.XPATH,"//input[@placeholder='Username']").send_keys("Admin")
-        self.driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("admin123")
-        self.driver.find_element(By.XPATH,"//button[normalize-space()='Login']").click()
-        actual_text = self.driver.find_element(By.XPATH,"//h6[normalize-space()='Dashboard']").text
-        assert_that("Dashboard").is_equal_to(actual_text)
-
