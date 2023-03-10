1f1473b4540db3c668386eb67bbafc1a880009cb
Maulin111 <Maulin.desai@einfochips.com>
2023-03-10 10:48:59 +0530

    Day14_worksession

--- Hybrid_framework/utilities/read_utils.py
+++ Hybrid_framework/utilities/read_utils.py
@@ -5,7 +5,6 @@ def get_csv_as_list(file_path):
     df = pandas.read_csv(filepath_or_buffer=file_path, delimiter=";")
     return df.values.tolist()
 
-
-#excel
-#json
-#excel
+def get_sheet_as_list(filepath, sheetname):
+    df = pandas.read_excel(io = filepath,sheet_name=sheetname)
+    return df.values.tolist()
